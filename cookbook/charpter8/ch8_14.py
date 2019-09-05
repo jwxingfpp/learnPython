c62c0ac21df6c5d34768a0b4b3b43b88b97825b9
Xing Jiawei <xingjiawei@megvii.com>
2019-09-05 20:44:31 +0800

    feat(8.14-2): 实现自定义的可迭代容器

--- cookbook/charpter8/ch8_14.py
+++ cookbook/charpter8/ch8_14.py
@@ -4,19 +4,35 @@
 @Author  : xingjiawei
 实现自定义容器
 """
-
+import bisect
 import collections
 
 
-class List(collections.Iterable):
-    def __init__(self):
-        self.children = range(1, 6)
+# 继承Sequence抽象类，并且实现元素按照顺序存储
+class SortedItems(collections.Sequence):
+    def __init__(self, initial=None):
+        self._items = sorted(initial) if initial else []
 
-    def __iter__(self):
-        return iter(self.children)
+    def __getitem__(self, index):
+        return self._items[index]
+
+    def __len__(self):
+        return len(self._items)
+
+    # Method for adding item in the right location
+    def add(self, item):
+        bisect.insort(self._items, item)
 
 
 if __name__ == '__main__':
-    l = List()
-    for a in l:
-        print a
+    s = SortedItems(range(2, 5))
+    print s
+    print list(s)
+    print s[0], s[-1]
+    s.add(5)
+    print list(s)
+    print '-'*10
+    print isinstance(s, collections.Sequence)
+    print isinstance(s, collections.Iterable)
+    print isinstance(s, collections.Container)
+    print isinstance(s, collections.Sized)
