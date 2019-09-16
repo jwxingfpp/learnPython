993e65afe5d6979aa640fd74f854910fa93c077b
Xing Jiawei <xingjiawei@megvii.com>
2019-09-16 09:47:56 +0800

    feat(ch9_1-1): 使用装饰器

---
+++ cookbook/charpter9/__init__.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+"""
+@Time    : 2019/9/16 上午9:35
+@Author  : xingjiawei
+"""
---
+++ cookbook/charpter9/ch9_1.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+"""
+@Time    : 2019/9/16 上午9:36
+@Author  : xingjiawei
+装饰器
+增加额外操作（如日志、计时等）
+"""
+import time
+from functools import wraps
+
+
+def timethis(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print '='*10, func.__name__, ' cost=', end-start
+        return result
+    return wrapper
+
+
+@timethis
+def countdown(n):
+    while n > 0:
+        n -= 1
+
+
+def count(n):
+    for i in range(n):
+        pass
+
+
+if __name__ == '__main__':
+    # 装饰器
+    countdown(15)
+    # 函数作为参数传入
+    wrapper_count = timethis(count)
+    wrapper_count(15)
+    print countdown
+    print wrapper_count
