47aaa85cdb6b6f260d5971b0a151f0bb20f46979
Xing Jiawei <xingjiawei@megvii.com>
2019-09-18 16:12:27 +0800

    feat(ch7_11):通过生成器、协程使得回调函数内联在函数中

--- cookbook/charpter7/ch7_10.py
+++ cookbook/charpter7/ch7_10.py
@@ -4,6 +4,8 @@
 @Author  : xingjiawei
 带额外状态信息的回调函数
 """
+from Queue import Queue
+from functools import wraps
 
 
 def print_result(result):
@@ -47,22 +49,60 @@ def make_handler1():
         print '[{}] Got: {}'.format(sequence, result)
 
 
+# 通过生成器和协程，可以使得回调函数内联在某个函数中。
+class Async(object):
+    def __init__(self, func, args):
+        self.func = func
+        self.args = args
+
+
+def inlined_async(func):
+    @wraps(func)
+    def wrapper(*args):
+        f = func(*args)
+        result_queue = Queue()
+        result_queue.put(None)
+
+        while True:
+            try:
+                result = result_queue.get()
+                a = f.send(result)      # 第一次相当于next()
+                apply_async(a.func, result_queue.put, *a.args)
+            except StopIteration as e:
+                print 'generator stop'
+                break
+    return wrapper
+
+
+@inlined_async
+def test():
+    r = yield Async(add, (1, 3))
+    print r
+    r = yield Async(add, ('hello', 'world'))
+    print r
+    for i in range(10):
+        r = yield Async(add, (i, i*2))
+        print r
+    print 'done'
+
+
 if __name__ == '__main__':
-    apply_async(add, print_result, 1, 2)
-    apply_async(add, print_result, 'hello', 'world')
-
-    # 使用类
-    rh = ResultHandler()
-    apply_async(add, rh.handler, 1, 2)
-    apply_async(add, rh.handler, 'hello', 'world')
-
-    # 闭包
-    handler = make_handler()
-    apply_async(add, handler, 1, 2)
-    apply_async(add, handler, 'hello', 'world')
-
-    # 协程
-    handle = make_handler1()
-    next(handle) # advance to the yield
-    apply_async(add, handle.send, 1, 2)
-    apply_async(add, handle.send, 'hello', 'world')
+    # apply_async(add, print_result, 1, 2)
+    # apply_async(add, print_result, 'hello', 'world')
+    #
+    # # 使用类
+    # rh = ResultHandler()
+    # apply_async(add, rh.handler, 1, 2)
+    # apply_async(add, rh.handler, 'hello', 'world')
+    #
+    # # 闭包
+    # handler = make_handler()
+    # apply_async(add, handler, 1, 2)
+    # apply_async(add, handler, 'hello', 'world')
+    #
+    # # 协程
+    # handle = make_handler1()
+    # next(handle) # advance to the yield
+    # apply_async(add, handle.send, 1, 2)
+    # apply_async(add, handle.send, 'hello', 'world')
+    test()
